402b5d808d39edba84bd1c9fdc751201697c7382
Flavio <mataqque.100@gmail.com>
2020-02-09 15:48:53 -0500

    complete

--- public/options.js
+++ public/options.js
@@ -1,25 +1,37 @@
 var contenido;
-document.getElementById("llamada").addEventListener("click",function llamada(){
+$(document).ready(()=>{
     $.ajax({
         url:"/public/oracioningles.json",
         method:"GET",
         success:function(data){
             contenido = data;
-            mostrar();
+            start_again();
         }
     })
 });
+document.getElementById("llamada").addEventListener("click",start_again);
 
 var random = function(){
     let numeral = Math.floor((Math.random()*contenido.length));
     return numeral; 
 }
 
-function mostrar(){
-    let num = contenido[random()];
-    document.getElementById("contenido_esp").textContent = num["español"];
+function start_again(){
+    if(document.getElementsByClassName("mostrar_eye")[0].id=="active"){
+        let num = contenido[random()];
+        document.getElementById("contenido_esp").textContent = num["español"];
+        document.getElementsByClassName("oracion_ingles")[0].textContent = num["ingles"];
+        document.getElementsByClassName("mostrar_eye")[0].id="inactive"
+    }
+    if(document.getElementsByClassName("mostrar_eye")[0].id=="inactive"){
+        document.getElementsByClassName("mostrar_eye")[0].style.display="block";
+        document.getElementsByClassName("oracion_ingles")[0].style.display="none";
+    }
+
 }
 document.getElementsByClassName("mostrar_eye")[0].addEventListener("click",function(){
-    document.getElementsByClassName("oracion_ingles")[0].style.setProperty("--none","block")
-    document.getElementsByClassName("mostrar_eye")[0].style.setProperty("--block","none");
-})
+    document.getElementsByClassName("oracion_ingles")[0].style.display="block";
+    document.getElementsByClassName("mostrar_eye")[0].style.display="none";
+    document.getElementsByClassName("mostrar_eye")[0].id="active";
+});
+
