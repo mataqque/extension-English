2f93afeaca58f0d408d6e70fcc99352fd33be25e
Flavio <mataqque.100@gmail.com>
2020-02-09 14:43:44 -0500

    css style

--- public/options.js
+++ public/options.js
@@ -1,10 +1,11 @@
+var contenido;
 document.getElementById("llamada").addEventListener("click",function llamada(){
     $.ajax({
         url:"/public/oracioningles.json",
         method:"GET",
         success:function(data){
-            console.log(data)
-            document.getElementById("contenedor-traduccion").textContent = data
+            contenido = data;
+            mostrar();
         }
     })
 });
@@ -13,3 +14,12 @@ var random = function(){
     let numeral = Math.floor((Math.random()*contenido.length));
     return numeral; 
 }
+
+function mostrar(){
+    let num = contenido[random()];
+    document.getElementById("contenido_esp").textContent = num["español"];
+}
+document.getElementsByClassName("mostrar_eye")[0].addEventListener("click",function(){
+    document.getElementsByClassName("oracion_ingles")[0].style.setProperty("--none","block")
+    document.getElementsByClassName("mostrar_eye")[0].style.setProperty("--block","none");
+})
